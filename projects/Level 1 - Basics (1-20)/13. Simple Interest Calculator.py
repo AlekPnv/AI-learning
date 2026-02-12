897419af9ffc653bbdc90fb8293050085323f294
Alek Penev <alek-aleksiev.penev0707@wfo-bruneck.info>
2026-02-12 20:11:05 +0100

    Add Simple Interest Calculator implementation

---
+++ projects/Level 1 - Basics (1-20)/13. Simple Interest Calculator.py
@@ -0,0 +1,38 @@
+def get_values(prompt) -> float:
+    while True:
+        num = input(prompt)
+        try:
+            num = float(num)
+            
+            if num <= 0:
+                print("Invalid Number! Must be greater than 0")
+                continue
+            
+            return num
+        
+        except ValueError:
+            print("Invalid Input! Must enter a valid number")
+
+
+def calculate_interest(amount, rate, time):
+    interest = (amount * rate * time) / 100
+    total_amount = amount + interest
+    return total_amount, interest
+
+def main():
+    while True:
+        amount = get_values("Initial amount: ")
+        annual_rate = get_values("Annual interest rate (%): ")
+        num_years = get_values("Time in years: ")
+        
+        interest, total_amount = calculate_interest(amount, annual_rate, num_years)
+        
+        print(f"\nInterest earned: {interest:.2f}€")
+        print(f"Total amount: {total_amount:.2f}€")
+        
+        if input("\nAgain? [y/n]: ").lower() != "y":
+            break
+
+
+if __name__ == "__main__":
+    main()
